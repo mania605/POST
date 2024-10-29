763185424ece03432d92495f1371e27309488e6b
mania605 <kim.minsun83@gmail.com>
2024-10-29 10:22:46 +0900

    순서정리 중요

--- post_app/views.py
+++ post_app/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+#순서2 url요청이 들어왔을 때 요청의 HTTP방식 확인해서 해당 방식에 맞는 DB데이터(모델)제어
 @api_view(['GET', 'POST'])
 def posts(request):
 
