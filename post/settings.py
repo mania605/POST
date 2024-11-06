603d21dda5238a112857ae6b1c98fe30853f4408
mania605 <kim.minsun83@gmail.com>
2024-11-06 18:06:04 +0900

    어젠됐잖아 오늘 왜안돼

--- post/settings.py
+++ post/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [  
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+        "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "https://post-1htn.onrender.com", 
 ]
 
 CORS_ALLOW_METHODS = [
